09ee1443a79d61fa488c86f927a14e01c5e21be2
HP <luis.cisneros.salas03@gmail.com>
2025-06-19 23:37:08 -0500

    "add endpoint empresa reniec"

--- src/main/java/com/cramirez/backendcramirez/reniec/infrastructure/web/ReniecController.java
+++ src/main/java/com/cramirez/backendcramirez/reniec/infrastructure/web/ReniecController.java
@@ -46,4 +46,40 @@ public class ReniecController {
                     .body("Error al consultar RENIEC: " + e.getMessage());
         }
     }
+
+    @GetMapping("/buscarEmpresa/{ruc}")
+    public ResponseEntity<String> buscarEmpresa(@PathVariable String ruc) {
+        final String API_SUNAT_URL = "https://api.apis.net.pe/v2/sunat/ruc/full?numero=";
+
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+
+            // Configurar cabeceras HTTP
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.set("Authorization", AUTHORIZATION_TOKEN);
+
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+            // Realizar solicitud a la API externa
+            ResponseEntity<String> response = restTemplate.exchange(
+                    API_SUNAT_URL + ruc,
+                    HttpMethod.GET,
+                    entity,
+                    String.class
+            );
+
+            if (response.getStatusCode().is2xxSuccessful()) {
+                return ResponseEntity.ok(response.getBody());
+            } else {
+                return ResponseEntity.status(response.getStatusCode()).body("Error al consultar SUNAT");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al consultar SUNAT: " + e.getMessage());
+        }
+    }
+
 }
