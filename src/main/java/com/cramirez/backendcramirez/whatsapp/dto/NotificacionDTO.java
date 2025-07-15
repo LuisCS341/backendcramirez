df5573c6adb44743efec60232f27ca1efab41f5d
HP <luis.cisneros.salas03@gmail.com>
2025-07-15 06:48:50 -0500

    "se elimino bot de wsp"

--- src/main/java/com/cramirez/backendcramirez/whatsapp/dto/NotificacionDTO.java
+++
@@ -1,6 +0,0 @@
-package com.cramirez.backendcramirez.whatsapp.dto;
-
-public class NotificacionDTO {
-    private String psid;      // ID del usuario en Facebook Messenger
-    private String mensaje;
-}
